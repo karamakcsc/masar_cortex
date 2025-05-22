e1c94435f3e3b3e40d0d7b0ea3a99f5df0bdf425
karamakcsc <karamakcsc@hotmail.com>
2025-05-22 14:37:57 +0300

    update item price

--- masar_cortex/masar_cortex/doctype/bulk_item_price_update/bulk_item_price_update.js
+++ masar_cortex/masar_cortex/doctype/bulk_item_price_update/bulk_item_price_update.js
@@ -1,11 +1,20 @@
 // Copyright (c) 2025, KCSC and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Bulk Item Price Update", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Bulk Item Price Update", {
+	validate: function(frm) {
+        if (frm.doc.items.length > 0) {
+            frm.doc.items.forEach(function(item) {
+                if (item.weight_per_unit) {
+                    item.new_price = item.rate_per_kg * item.weight_per_unit;
+                } else {
+                    frappe.throw("Weight per unit is required for all items.");
+                }
+            });
+            frm.refresh_field('items');
+        }
+	},
+});
 
 frappe.ui.form.on("Bulk Item Price Item", {
 	rate_per_kg: function(frm, cdt, cdn) {
@@ -16,7 +25,6 @@ frappe.ui.form.on("Bulk Item Price Item", {
 
 function calcRate(frm, cdn, cdt) {
     var row = locals[cdt][cdn];
-    console.log("Nice");
     row.new_price = row.rate_per_kg * row.weight_per_unit;
     frm.refresh_field('items');
 }
